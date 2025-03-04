5cc8b65a10d49ea74a448780f10e9e46a58e090c
dreyes <phoenixdhr@gmail.com>
2025-03-04 18:13:45 -0500

    instlacion de npx astro add node y configuracion en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  server: {
+          host: '0.0.0.0'
+          },
   adapter: node({
     mode: 'standalone'
   })
+
+
 });
