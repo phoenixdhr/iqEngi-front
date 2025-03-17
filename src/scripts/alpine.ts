6979ca011227f5dab44ba3ed38d10f2fc9341c1b
dreyes <phoenixdhr@gmail.com>
2025-03-17 18:28:36 -0500

    se instalo alpinejs y @types/alpinejs, asi mismo se modificó el archivo Navbar, para facilitar su importacion y no depender de CDN externas

---
+++ src/scripts/alpine.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine; // Asigna Alpine a window para que esté disponible globalmente
+
+Alpine.start(); // Inicia Alpine.js
